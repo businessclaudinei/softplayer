f38afd2a17822d7d6ac1429696b88fcbe09de078
Claudinei <dine_niu@hotmail.com>
2020-06-28 18:30:51 -0300

    Instalação do pacote StackExchange.Redis da Microsoft

--- src/Accounting.Interest/Accounting.Interest.Domain/Commands/CalculateInterest/CalculateInterestCommandHandler.cs
+++ src/Accounting.Interest/Accounting.Interest.Domain/Commands/CalculateInterest/CalculateInterestCommandHandler.cs
@@ -1,4 +1,7 @@
-﻿using MediatR;
+﻿using Accounting.Interest.Infrastruture.Service.Resources.Cache;
+using MediatR;
+using Newtonsoft.Json;
+using System;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,13 +9,30 @@ namespace Accounting.Interest.Domain.Commands.CalculateInterest
 {
     public class CalculateInterestCommandHandler : IRequestHandler<CalculateInterestCommand, CalculateInterestCommandResponse>
     {
-        public CalculateInterestCommandHandler()
+        private readonly IResponseCacheService _responseCacheService;
+
+        public CalculateInterestCommandHandler(IResponseCacheService responseCacheService)
         {
+            _responseCacheService = responseCacheService;
         }
 
         public async Task<CalculateInterestCommandResponse> Handle(CalculateInterestCommand command, CancellationToken cancellation)
         {
-            return new CalculateInterestCommandResponse { };
+            decimal compoundInterestAmount = 0;
+
+            var cachedValue = await _responseCacheService.GetCachedResponseAsync("test");
+
+            if (cachedValue != null)
+            {
+                compoundInterestAmount = JsonConvert.DeserializeObject<decimal>(cachedValue);
+                await _responseCacheService.CacheResponseAsync("test", compoundInterestAmount++, new TimeSpan(0, 0, 20)).ConfigureAwait(false);
+            }
+            else
+            {
+                await _responseCacheService.CacheResponseAsync("test", 1, new TimeSpan(0, 0, 20)).ConfigureAwait(false);
+            }
+
+            return new CalculateInterestCommandResponse { CompoundInterestAmount = compoundInterestAmount };
         }
     }
 }
